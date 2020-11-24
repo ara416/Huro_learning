0459c85df4c4c948d42b35d92af600b088dbb3ad
ara416 <ara416@lehigh.edu>
2020-11-24 11:40:36 -0500

    Moving toward the goal and our put data is added.

--- Game/Game.py
+++ Game/Game.py
@@ -2,29 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Automata import dfa
 from Gamedata import *
-d=2
-xh=np.array([1,2,1])
 
-
+xh=np.array([1,2,1])# Human State
+xf=np.array([0,3,2])# The destination result
+d=xf[2]
+S[xf[0],xf[1]]=1# Shows out door
+S[xf[0],xf[1]+1]=1# Shows out door
 Sp=np.copy(S)
-Sp[xh[0],xh[1]]=3
+Sp[xh[0],xh[1]]=3# Shows human on map
 xr=move(xh,-2)
-Sp[xr[0],xr[1]]=2
+Sp[xr[0],xr[1]]=2# Shows robot on the map
+xt=xh# Stacked array
 print('=========================Initail ===========================')
 print(Sp)
 Ins=''
+Inro=''
 ac=action(xh[0],xh[1],S)
 print(np.argmax(moveprob(xh,ac,S)))
 while True:
     Sp=np.copy(S)
-    while True:
-        robo=input("Enter robot actoin f or r: \t")
-        if robo=='r'or robo=='f':
-            break
-        else:
-            print('Please input f or r')
     df=dirflag(xh,d)
     rf=rotflag(xh,S)
+    while True:
+        robo=input("Enter robot actoin f or r: \t")
+        if  robo=='f':
+            break
+        elif robo=='r' and rf:
+            break
+        else:
+            print('Wrong input (check if rotation is possibl)')
     Ins=Ins+(trIn(rf,df))
     ac=(action(xh[0],xh[1],S))
     DfaOut=dfa.read_input(Ins)
@@ -54,12 +60,25 @@ while True:
                 print('Rotation is not possible')
             rf=rotflag(xh,S)
             df=dirflag(xh,d)
+        else:
+            print('moving Toward Out Door')
+            md=xf-xh
+            print(md)
+            mds=np.sign(md)
+            mda=np.argmax(np.abs(md[0:2]))
+            xh[mda]=xh[mda]+mds[mda]
+            if all(xf==xh):
+                print('-------------------------------------------------------------------------Finished!')
+                break
         #break
     Sp[xh[0],xh[1]]=3
     xr=move(xh,-2)
     Sp[xr[0],xr[1]]=2
     Ins=Ins+(trIn(rf,df))
+    Inro=Inro+robo
     DfaOut=dfa.read_input(Ins)
+    xt=np.vstack((xt,xh))
+    print(xt,Inro)
     print(Sp)
     print('DF','RF','DFA','Input','xh',sep='\t')
     print(df,rf,DfaOut,robo,xh,sep='\t')
--- Game/Gamedata.py
+++ Game/Gamedata.py
@@ -1,5 +1,5 @@
 import numpy as np
-from Automata import dfa 
+from Automata import dfa
 
 
 ni=10
